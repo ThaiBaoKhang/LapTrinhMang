781267c6b6e43cee78ebb2ebd2228cc3cd2165d8
ThaiBaoKhang <thaibk69@gmail.com>
2018-04-30 21:49:42 +0700

    Thêm buổi 6 ma trận (chưa xong)

---
+++ ThucHanhBuoi6/MaTran-Client/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace MaTran_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chonsodong1 = new System.Windows.Forms.NumericUpDown();
+            this.chonsocot1 = new System.Windows.Forms.NumericUpDown();
+            this.chonsocot2 = new System.Windows.Forms.NumericUpDown();
+            this.chonsodong2 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.grbnhap = new System.Windows.Forms.GroupBox();
+            this.btnTaoMaTran1 = new System.Windows.Forms.Button();
+            this.btnTaoMaTran2 = new System.Windows.Forms.Button();
+            this.grbnhap2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.grbketqua = new System.Windows.Forms.GroupBox();
+            this.txbTest = new System.Windows.Forms.TextBox();
+            this.btnGiai = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chonsodong1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chonsocot1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chonsocot2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chonsodong2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MA TRẬN 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dòng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Cột";
+            // 
+            // chonsodong1
+            // 
+            this.chonsodong1.Location = new System.Drawing.Point(62, 64);
+            this.chonsodong1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.chonsodong1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.chonsodong1.Name = "chonsodong1";
+            this.chonsodong1.Size = new System.Drawing.Size(32, 20);
+            this.chonsodong1.TabIndex = 5;
+            this.chonsodong1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // chonsocot1
+            // 
+            this.chonsocot1.Location = new System.Drawing.Point(139, 64);
+            this.chonsocot1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.chonsocot1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.chonsocot1.Name = "chonsocot1";
+            this.chonsocot1.Size = new System.Drawing.Size(32, 20);
+            this.chonsocot1.TabIndex = 6;
+            this.chonsocot1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // chonsocot2
+            // 
+            this.chonsocot2.Location = new System.Drawing.Point(434, 64);
+            this.chonsocot2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.chonsocot2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.chonsocot2.Name = "chonsocot2";
+            this.chonsocot2.Size = new System.Drawing.Size(32, 20);
+            this.chonsocot2.TabIndex = 11;
+            this.chonsocot2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // chonsodong2
+            // 
+            this.chonsodong2.Location = new System.Drawing.Point(357, 64);
+            this.chonsodong2.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.chonsodong2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.chonsodong2.Name = "chonsodong2";
+            this.chonsodong2.Size = new System.Drawing.Size(32, 20);
+            this.chonsodong2.TabIndex = 10;
+            this.chonsodong2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(405, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Cột";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(318, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Dòng";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(341, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 24);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "MA TRẬN 2";
+            // 
+            // grbnhap
+            // 
+            this.grbnhap.Location = new System.Drawing.Point(12, 103);
+            this.grbnhap.Name = "grbnhap";
+            this.grbnhap.Size = new System.Drawing.Size(214, 174);
+            this.grbnhap.TabIndex = 12;
+            this.grbnhap.TabStop = false;
+            this.grbnhap.Text = "Nhập dữ liệu";
+            // 
+            // btnTaoMaTran1
+            // 
+            this.btnTaoMaTran1.Location = new System.Drawing.Point(176, 20);
+            this.btnTaoMaTran1.Name = "btnTaoMaTran1";
+            this.btnTaoMaTran1.Size = new System.Drawing.Size(50, 33);
+            this.btnTaoMaTran1.TabIndex = 13;
+            this.btnTaoMaTran1.Text = "Tạo";
+            this.btnTaoMaTran1.UseVisualStyleBackColor = true;
+            this.btnTaoMaTran1.Click += new System.EventHandler(this.btnTaoMaTran1_Click);
+            // 
+            // btnTaoMaTran2
+            // 
+            this.btnTaoMaTran2.Location = new System.Drawing.Point(470, 22);
+            this.btnTaoMaTran2.Name = "btnTaoMaTran2";
+            this.btnTaoMaTran2.Size = new System.Drawing.Size(50, 33);
+            this.btnTaoMaTran2.TabIndex = 14;
+            this.btnTaoMaTran2.Text = "Tạo";
+            this.btnTaoMaTran2.UseVisualStyleBackColor = true;
+            this.btnTaoMaTran2.Click += new System.EventHandler(this.btnTaoMaTran2_Click);
+            // 
+            // grbnhap2
+            // 
+            this.grbnhap2.Location = new System.Drawing.Point(306, 103);
+            this.grbnhap2.Name = "grbnhap2";
+            this.grbnhap2.Size = new System.Drawing.Size(214, 174);
+            this.grbnhap2.TabIndex = 15;
+            this.grbnhap2.TabStop = false;
+            this.grbnhap2.Text = "Nhập dữ liệu";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(256, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 33);
+            this.label7.TabIndex = 0;
+            this.label7.Text = ".";
+            // 
+            // grbketqua
+            // 
+            this.grbketqua.Location = new System.Drawing.Point(62, 323);
+            this.grbketqua.Name = "grbketqua";
+            this.grbketqua.Size = new System.Drawing.Size(404, 138);
+            this.grbketqua.TabIndex = 17;
+            this.grbketqua.TabStop = false;
+            this.grbketqua.Text = "Kết quả";
+            // 
+            // txbTest
+            // 
+            this.txbTest.Location = new System.Drawing.Point(377, 291);
+            this.txbTest.Name = "txbTest";
+            this.txbTest.Size = new System.Drawing.Size(106, 20);
+            this.txbTest.TabIndex = 18;
+            // 
+            // btnGiai
+            // 
+            this.btnGiai.Location = new System.Drawing.Point(190, 283);
+            this.btnGiai.Name = "btnGiai";
+            this.btnGiai.Size = new System.Drawing.Size(140, 34);
+            this.btnGiai.TabIndex = 19;
+            this.btnGiai.Text = "Giải";
+            this.btnGiai.UseVisualStyleBackColor = true;
+            this.btnGiai.Click += new System.EventHandler(this.btnGiai_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(552, 470);
+            this.Controls.Add(this.btnGiai);
+            this.Controls.Add(this.txbTest);
+            this.Controls.Add(this.grbketqua);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.grbnhap2);
+            this.Controls.Add(this.btnTaoMaTran2);
+            this.Controls.Add(this.btnTaoMaTran1);
+            this.Controls.Add(this.grbnhap);
+            this.Controls.Add(this.chonsocot2);
+            this.Controls.Add(this.chonsodong2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.chonsocot1);
+            this.Controls.Add(this.chonsodong1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chonsodong1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chonsocot1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chonsocot2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chonsodong2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown chonsodong1;
+        private System.Windows.Forms.NumericUpDown chonsocot1;
+        private System.Windows.Forms.NumericUpDown chonsocot2;
+        private System.Windows.Forms.NumericUpDown chonsodong2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox grbnhap;
+        private System.Windows.Forms.Button btnTaoMaTran1;
+        private System.Windows.Forms.Button btnTaoMaTran2;
+        private System.Windows.Forms.GroupBox grbnhap2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.GroupBox grbketqua;
+        private System.Windows.Forms.TextBox txbTest;
+        private System.Windows.Forms.Button btnGiai;
+    }
+}
+
---
+++ ThucHanhBuoi6/MaTran-Client/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace MaTran_Client
+{
+    public partial class Form1 : Form
+    {        
+        int z = 0;
+        int lk = 0;
+        int dem = 0;
+        int dem2 = 0;
+        int[,] mt1;
+        int[,] mt2;
+
+        Socket server;
+        IPEndPoint ipe;
+        Thread ketnoi;
+        TcpClient clientSocket = new TcpClient();
+
+        public Form1()
+        {
+            InitializeComponent();      
+        }
+
+        private void btnTaoMaTran1_Click(object sender, EventArgs e)
+        {
+            if (chonsocot1.Value != chonsodong2.Value)
+            {
+                MessageBox.Show("Số cột ma trận 1 phải bằng số dòng ma trận 2!");
+            }
+            else
+            {             
+                int chieucaotxb = 20;
+                int chieudaitxb = 19;
+                int khoangcach = 10;
+                int a = 6;
+                int b = 19;
+                int c = 0;
+                int i = (int)chonsodong1.Value;
+                int j = (int)chonsocot1.Value;              
+                for (int y = 0; y < i; y++)
+                {
+                    b = 19;
+                    a = a + 10;
+                    for (int x = 0; x < j; x++)
+                    {
+                        TextBox txb = new TextBox();
+                        txb.Top = a + (y * chieucaotxb + khoangcach) + 5;
+                        txb.Left = b + (x * chieudaitxb + khoangcach) + 5;
+                        b = b + 10;
+                        txb.Name = "txb" + c;
+                        z = c;
+                        c++;
+                        txb.Width = chieudaitxb;
+                        txb.Height = chieucaotxb;
+                        grbnhap.Controls.Add(txb);                      
+                    }
+                }
+                chonsodong1.Enabled = false;
+                chonsocot1.Enabled = false;
+            }
+        }
+
+        private void btnTaoMaTran2_Click(object sender, EventArgs e)
+        {
+            if (chonsocot1.Value != chonsodong2.Value)
+            {
+                MessageBox.Show("Số cột ma trận 1 phải bằng số dòng ma trận 2!");
+            }
+            else
+            {
+                int chieucaotxb = 20;
+                int chieudaitxb = 19;
+                int khoangcach = 10;
+                int a = 6;
+                int b = 19;
+                int c = 0;
+                int i = (int)chonsodong2.Value;
+                int j = (int)chonsocot2.Value;
+                for (int y = 0; y < i; y++)
+                {
+                    b = 19;
+                    a = a + 10;
+                    for (int x = 0; x < j; x++)
+                    {
+                        TextBox txb = new TextBox();
+                        txb.Top = a + (y * chieucaotxb + khoangcach) + 5;
+                        txb.Left = b + (x * chieudaitxb + khoangcach) + 5;
+                        b = b + 10;
+                        txb.Name = "txc" + c;
+                        lk = c;
+                        c++;
+                        txb.Width = chieudaitxb;
+                        txb.Height = chieucaotxb;
+                        grbnhap2.Controls.Add(txb);
+                    }
+                }
+                chonsodong2.Enabled = false;
+                chonsocot2.Enabled = false;
+            }
+        }
+        // luu mang
+        private void Test_Click(object sender, EventArgs e)
+        {         
+           int i = (int)chonsodong1.Value;
+            int j = (int)chonsocot1.Value;            
+            mt1 = new int[i, j];            
+            for (int r = 0; r < i; r++)
+            {                
+                for (int l = 0; l < j; l++)
+                {
+                    if (dem <= z)
+                    {
+                        mt1[r, l] = Convert.ToInt32(((TextBox)grbnhap.Controls["txb" + dem]).Text);
+                        
+                        dem++;
+                    }                  
+                }
+            }
+
+            int k = (int)chonsodong2.Value;
+            int s = (int)chonsocot2.Value;
+            mt2 = new int[k, s];
+
+            for (int r = 0; r < k; r++)
+            {
+                for (int l = 0; l < s; l++)
+                {
+                    if (dem2 <= lk)
+                    {                       
+                        mt2[r, l] = Convert.ToInt32(((TextBox)grbnhap2.Controls["txc" + dem2]).Text);
+                        dem2++;
+                    }
+                }
+            }          
+        }           
+
+        private void btnGiai_Click_1(object sender, EventArgs e)
+        {
+            int i = (int)chonsodong1.Value;
+            int j = (int)chonsocot1.Value;
+            mt1 = new int[i, j];
+            for (int r = 0; r < i; r++)
+            {
+                for (int l = 0; l < j; l++)
+                {
+                    if (dem <= z)
+                    {
+                        mt1[r, l] = Convert.ToInt32(((TextBox)grbnhap.Controls["txb" + dem]).Text);
+
+                        dem++;
+                    }
+                }
+            }
+
+            int k = (int)chonsodong2.Value;
+            int s = (int)chonsocot2.Value;
+            mt2 = new int[k, s];
+
+            for (int r = 0; r < k; r++)
+            {
+                for (int l = 0; l < s; l++)
+                {
+                    if (dem2 <= lk)
+                    {
+                        mt2[r, l] = Convert.ToInt32(((TextBox)grbnhap2.Controls["txc" + dem2]).Text);
+                        dem2++;
+                    }
+                }
+            }
+            int rowsmatrix1 = mt1.GetLength(0);
+            int columnmatrix1 = mt1.GetLength(1);
+            int rowsmatrix2 = mt2.GetLength(0);
+            int columnmatrix2 = mt2.GetLength(1);
+            try
+            {
+                NetworkStream serverStream = clientSocket.GetStream();               
+                byte[] outStream = Encoding.ASCII.GetBytes("Ma tran 1 co " + rowsmatrix1 + " dong " + columnmatrix1 + " cot$");            
+                byte[] outStream2 = Encoding.ASCII.GetBytes("Ma tran 2 co " + rowsmatrix2 + " dong " + columnmatrix2 + " cot$");              
+                serverStream.Write(outStream, 0, outStream.Length);
+                serverStream.Write(outStream2, 0, outStream.Length);             
+                serverStream.Flush();
+
+                byte[] inStream = new byte[1024];
+                serverStream.Read(inStream, 0, inStream.Length);
+                string returndata = Encoding.ASCII.GetString(inStream);
+                txbTest.Text = "Data from Server : " + returndata;
+            }
+            catch 
+            {
+                MessageBox.Show("Kết nối đến server đã bị đóng! Thoát chương trình ");
+                this.Close();
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                clientSocket.Connect("127.0.0.1", 9050);
+            }
+            catch
+            {
+                MessageBox.Show("Không thể kết nối đến server");
+                this.Close();
+                return;                
+            }
+            
+        }
+    }
+}
+  
+
